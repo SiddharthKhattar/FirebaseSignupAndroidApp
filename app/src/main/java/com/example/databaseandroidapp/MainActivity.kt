923158e72aaeeff8a6dd97d101c32842c604278a
SiddharthKhattar <skhattar111@gmail.com>
2024-07-20 01:40:04 +0530

    first commit

---
+++ app/src/main/java/com/example/databaseandroidapp/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.databaseandroidapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var database : DatabaseReference
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val signButton = findViewById<Button>(R.id.btnSignUp)
+
+        val etName = findViewById<EditText>(R.id.etName)
+        val etMail = findViewById<EditText>(R.id.etMail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val etUserName = findViewById<EditText>(R.id.etUserName)
+
+        signButton.setOnClickListener{
+
+            val name = etName.text.toString()
+            val mail = etMail.text.toString()
+            val uniqueId = etUserName.text.toString()
+            val password = etPassword.text.toString()
+
+            val user = User(name,mail,password, uniqueId)
+
+            database = FirebaseDatabase.getInstance().getReference("Users")
+
+            database.child(uniqueId).setValue(user).addOnSuccessListener {
+                Toast.makeText(this,"User Registered", Toast.LENGTH_SHORT).show()
+            }.addOnSuccessListener {
+                Toast.makeText(this,"Failed", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        }
+    }
